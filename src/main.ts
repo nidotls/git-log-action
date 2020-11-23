6206317918746f83f49168a6cad3f721c65ac83a
thenilsdev <contact@ni.ls>
2020-11-23 15:04:34 +0100

    Finish MVP

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,62 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import simpleGit, {SimpleGit} from 'simple-git'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const git: SimpleGit = simpleGit()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const tags = await git.tags()
 
-    core.setOutput('time', new Date().toTimeString())
+    if (tags.all.length < 1) {
+      core.setFailed(`No tags available: ${JSON.stringify(tags)}`)
+      return
+    }
+
+    const previousTag =
+      tags.all.length < 2 ? null : tags.all[tags.all.length - 2]
+    const latestTag = tags.latest
+
+    let commits
+
+    if (previousTag == null) {
+      commits = await git.log({
+        to: latestTag,
+        format: {
+          abbrev: '%h',
+          author: '@%an',
+          message: '%s'
+        },
+        splitter: '\n',
+        multiLine: false
+      })
+    } else {
+      commits = await git.log({
+        from: previousTag,
+        to: latestTag,
+        format: {
+          abbrev: '%h',
+          author: '@%an',
+          message: '%s'
+        },
+        splitter: '\n',
+        multiLine: false
+      })
+    }
+
+    let textLog = ''
+    let markdownLog = ''
+
+    for (const commit of commits.all) {
+      textLog += `${commit.abbrev} - ${commit.author} - ${commit.message}\n`
+      markdownLog += `[${commit.abbrev}](${process.env.GITHUB_REPOSITORY}/${process.env.GITHUB_REPOSITORY}/commit/${commit.abbrev}) ${commit.author} - ${commit.message}\n`
+    }
+
+    core.info(`previousTag: ${previousTag}`)
+    core.info(`latestTag: ${latestTag}`)
+    core.info(textLog)
+
+    core.setOutput('log', textLog)
+    core.setOutput('markdownLog', markdownLog)
   } catch (error) {
     core.setFailed(error.message)
   }
